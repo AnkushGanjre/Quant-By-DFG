cba31f44371da602f9e2eed11c6a8e6a28bb7868
Ankush Ganjre <ankushganjre@gmail.com>
2024-12-25 02:02:57 +0530

    logo, & pages

---
+++ pages/1_Dashboard.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+# Check if user is logged in
+if "authToken" not in st.session_state:
+    st.warning("Please log in to access the Dashboard.")
+    st.switch_page("main.py")
+
+# Display dashboard content
+st.title("Dashboard")
+st.write("Welcome to Algo by DFG!")
+
+# Display user profile data
+st.write("Profile Data:", st.session_state.profile_data)
